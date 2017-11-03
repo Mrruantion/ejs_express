9c7b3bedaec94005c6742c5333893a148a61b479
mrruantion <18344178461@163.com>
2017-11-03 18:09:29 +0800

    change

--- routes/oil_stat.js
+++ routes/oil_stat.js
@@ -1,39 +1,58 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/',function(req,res,next){
-    res.render('oil_stat',{title:'油耗统计'})
+var plate = require('../config');
+router.get('/', function (req, res, next) {
+    res.render('oil_stat', { title: '油耗统计' })
 })
 
-router.get('/getoil',function(req,res,next){
-    // console.log(req)
-    // console.log(req.con)
-    var db = req.con;
-    let dd = req.query;
-    var db = req.con;
-    var data = "";
 
-    var user = "";
-    var user = req.query.user;
-
-    var filter = "";
-    if (user) {
-        filter = 'WHERE userid = ?';
-    }
-    db.query('SELECT * FROM account ' + filter, user, function(err, rows) {
-        if (err) {
-            console.log(err);
+router.get('/getoil', function (req, res, next) {
+    var db = req.con;
+    var query = req.query.data;
+    let url = '';
+    for (var o in query) {
+        if (o == 'TripStartTime') {
+            url += o + ">'" + query[o] + "' and "
+        } else if (o == 'TripEndTime') {
+            url += o + "<'" + query[o] + "' and "
+        } else {
+            url += o + " = '" + query[o] + "' and "
         }
+    }
+    url = url.slice(0, -4)
+    var sql = 'SELECT * FROM trip_info WHERE ' + url;
+    db.query(sql, function (err, rows) {
         var data = rows;
-
-        // use index.ejs
-        // res.render('index', { title: 'Account Information', data: data, user: user });
-        res.json({ title: 'Account Information', data: data, user: user })
+        var dd = {
+            data: data,
+            allDate: data,
+            plate:plate
+        }
+        res.json(dd)
     });
-    // res.json({dfdf:33,reqss:dd})
 })
-// router.get("/",function(req,res,next){
-//     console.log(req,res,next)
-// })
-// router.get("")
+
+
+// function getBeforeDate(n,now) {
+//     var n = n;
+//     var d = new Date();
+//     var year = d.getFullYear();
+//     var mon = d.getMonth() + 1;
+//     var day = d.getDate();
+//     if (day <= n) {
+//         if (mon > 1) {
+//             mon = mon - 1;
+//         }
+//         else {
+//             year = year - 1;
+//             mon = 12;
+//         }
+//     }
+//     d.setDate(d.getDate() - n);
+//     year = d.getFullYear();
+//     mon = d.getMonth() + 1;
+//     day = d.getDate();
+//     s = year + "-" + (mon < 10 ? ('0' + mon) : mon) + "-" + (day < 10 ? ('0' + day) : day);
+//     return s;
+// }
 module.exports = router;
--- routes/trip_record.js
+++ routes/trip_record.js
@@ -1,8 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
+var plate = require('../config')
 router.get('/',function(req,res,next){
     res.render('trip_record',{title:'行程记录'})
 })
 
+router.get('/get_trip', function (req, res, next) {
+    var db = req.con;
+    var query = req.query.data;
+    let url = '';
+    for (var o in query) {
+        if (o == 'TripStartTime') {
+            url += o + ">'" + query[o] + "' and "
+        } else if (o == 'TripEndTime') {
+            url += o + "<'" + query[o] + "' and "
+        } else {
+            url += o + " = '" + query[o] + "' and "
+        }
+    }
+    url = url.slice(0, -4)
+    var sql = 'SELECT * FROM trip_info WHERE ' + url;
+    var data = "";
+    // var df = Object.assign({},db)
+    console.log(sql);
+    let pagenum = req.query.page;
+    let pageSize = 10;
+    sql2 = sql + "limit " + pagenum * pageSize + ' ,' + pageSize;
+    db.query(sql, function (err, rows) {
+        var total = rows.length;
+        var totalPage = parseInt(rows.length / pageSize);
+        if (total > pageSize) {
+            if (total % pageSize > 0) {
+                totalPage += 1
+            }
+        }
+        var data = rows;
+        if (total > pageSize) {
+            db.query(sql2, function (err, rowss) {
+                if (err) throw err;
+                var dd = {
+                    total: total,
+                    pageSize: pageSize,
+                    totalPage: totalPage,
+                    data: rowss,
+                    allDate: data,
+                    plate:plate
+                }
+                res.json(dd)
+            })
+        } else {
+            var dd = {
+                total: total,
+                pageSize: pageSize,
+                totalPage: totalPage,
+                data: data,
+                allDate: data,
+                plate:plate
+            }
+            // console.log(dd)
+            res.json(dd)
+        }
+    });
+})
 module.exports = router;
